535b5abe95ca9ec4e0c03b2715f92c8a78dca364
Dreamer <dreamerandprogrammer@gmail.com>
2021-02-14 14:15:35 +0200

    Add processing HTML

--- main.py
+++ main.py
@@ -1,56 +1,62 @@
 from css_html_js_minify import process_single_html_file, process_single_js_file, process_single_css_file, html_minify, js_minify, css_minify
 
-html = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Case</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body> 
-<div class="container">
-  <h2>Well</h2>
-  <div class="well">Basic Well</div>
-</div>
-</body>
-</html>
-"""
+def process_text(text):
+	output = "\""
+	splitLines = False
+	escapePercent = False
 
-minified = html_minify(html)
-# print(minified)
-
-output = "\""
-splitLines = False
-escapePercent = False
-
-for s in html:
-	if s == '\f':
-		output += "\\f"
-	elif s == '\n':
-		if splitLines:
-			output += "\\n\"\n\"";
+	for s in text:
+		if s == '\f':
+			output += "\\f"
+		elif s == '\n':
+			if splitLines:
+				output += "\\n\"\n\"";
+			else:
+				output += "\\n";
+		elif s == '\r':
+			output += "\\r"
+		elif s == '\t':
+			output += "\\t"
+		elif s == '\"':
+			output += "\\\""
+		elif s == '\\':
+			output += "\\\\"
+		elif s == '%':
+			if escapePercent:
+				output += "%%"
+			else:
+				output += "%"
 		else:
-			output += "\\n";
-	elif s == '\r':
-		output += "\\r"
-	elif s == '\t':
-		output += "\\t"
-	elif s == '\"':
-		output += "\\\""
-	elif s == '\\':
-		output += "\\\\"
-	elif s == '%':
-		if escapePercent:
-			output += "%%"
-		else:
-			output += "%"
-	else:
-		output += s
+			output += s
 
-output += "\""
+	output += "\""
 
-print(output)
+	return output
+
+import glob
+
+files_list_html = glob.glob('./*.html')
+print("html:")
+print(files_list_html)
+
+files_list_css = glob.glob('./*.css')
+print("css:")
+print(files_list_css)
+
+files_list_js = glob.glob('./*.js')
+print("js:")
+print(files_list_js)
+
+files_list_all = files_list_html + files_list_css + files_list_js
+
+print(" --- ")
+
+for i in files_list_html:
+	print("File: "+i)
+	f = open(i, "r")
+
+	text = f.read()
+	text = html_minify(text)
+
+	print(process_text(text))
+	print(" --- ")
